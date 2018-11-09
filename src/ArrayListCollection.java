71da77710ddcfc22f433d5c7d3ae56624333f709
JJ Hugh <hug17010@byui.edu>
2018-11-09 14:37:08 -0600

    Updated Portfolia FIles

--- src/ArrayListCollection.java
+++ src/ArrayListCollection.java
@@ -14,19 +14,37 @@ public class ArrayListCollection {
     
 
 
+   private static Scanner j = new Scanner (System.in);
+   private static int[] intData = new int[8];
+   
    public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    ArrayList<Integer> list = new ArrayList<>();
-    System.out.print("Enter integers please ");
-    System.out.println("(EOF or non-integer to terminate): ");
-
-    while(scan.hasNextInt()){
-         list.add(scan.nextInt());
+       System.out.println("Please enter 8 integers of your choice. ");
+       getInput();
+       outputArray(intData);
+       sizeArray();
+       System.out.println("Please Enter 10 integers of your choice. ");
+       getInput();
+       outputArray(intData);
     }
-
-    Integer [] nums = list.toArray(new Integer[0]);
-    for(int i = 0; i < nums.length; i++){
-       System.out.println(nums[i]);
-    }
+   
+   private static void getInput(){
+       for(int i = 0; i < intData.length; i++)
+           intData[i] = j.nextInt();
   }   
+   
+    private static void outputArray(int[] arr){
+       for(int i = 0; i < arr.length; i++) {
+           System.out.print(arr[i] + " ");
+       }
+       System.out.println();
+    }
+    
+     private static void sizeArray(){
+       int[] first = intData;
+       
+       intData = new int[10];
+       for (int i = 0; i < first.length; i++) {
+           intData[i] = first[i];
+       }
+}
 }
