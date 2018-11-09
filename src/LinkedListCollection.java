beec5d80c483ae3fec02e865e448acee4ae20f42
JJ Hugh <hug17010@byui.edu>
2018-11-09 11:01:33 -0600

    Updated Portfolia FIles

--- src/LinkedListCollection.java
+++ src/LinkedListCollection.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 public class LinkedListCollection {
 
-        @SuppressWarnings("unchecked")
+        //The main class that allows the program to run.
 	public static void main(String[] args) {
 		
 		System.out.println( "\nI love my kids so very much" ); 
 		
+                
 		List children = new LinkedList<>();
 
-children.add("Networking");
-children.add("and Server Administration");
-children.add(0, "Cyber Security");
+                    children.add("Kiana Marie");
+                    children.add("Pyper Lynn");
+                    children.add(0, "Paizley Jane");
+                    children.add(3, "Jordyn Elsie");
 
-	      System.out.println("My Name is Jonathan and I like: ");
+	      System.out.println("\nMy kids names are ");
 	      System.out.println(children);     
 					
 	}
