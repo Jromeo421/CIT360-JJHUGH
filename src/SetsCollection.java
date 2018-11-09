614fd7af3fe7625eb77ba3c868c203a11e5a8b69
JJ Hugh <hug17010@byui.edu>
2018-11-09 11:11:50 -0600

    Updated Portfolia FIles

--- src/SetsCollection.java
+++ src/SetsCollection.java
@@ -4,14 +4,14 @@ public class SetsCollection {
 
   public static void main(String args[]) { 
      int count[] = {5,10,15,20,25,30};
-     Set<Integer> set = new HashSet<>();
+     Set<Integer> setList = new HashSet<Integer>();
      try{
         for(int i = 0; i < 10; i++){
-           set.add(count[i]);
+           setList.add(count[i]);
         }
-        System.out.println(set);
+        System.out.println(setList);
   
-        TreeSet<Integer> sortedSet = new TreeSet<>(set);
+        TreeSet<Integer> sortedSet = new TreeSet<Integer>(setList);
         System.out.println("The sorted List is " + sortedSet);
       
 
